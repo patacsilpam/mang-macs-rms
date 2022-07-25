1f4e2a90fc2225a230f77f6a1c29c5618b00d0ae
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2022-07-25 21:52:15 +0800

    Update admin-signup.php

--- public/admin-signup.php
+++ public/admin-signup.php
@@ -29,8 +29,8 @@ function updateVerificationCode(){
                         $mail->isSMTP();
                         $mail->Host = 'smtp.gmail.com';
                         $mail->SMTPAuth = true;
-                        $mail->Username = 'mangmacsmarinerospizzahouse@gmail.com';
-                        $mail->Password = 'uihz grau bhyt qikw';
+                        $mail->Username = '';
+                        $mail->Password = '';
                         $mail->SMTPSecure = 'tls';
                         $mail->Port = 587;
                         $mail->setFrom('mangmacsmarinerospizzahouse@gmail.com', "Mang Mac's Marinero");
