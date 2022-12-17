f513c6606b73ea7f96405a6e3e2994b1161e4bbb
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2022-12-17 15:53:57 +0800

    Update mail-order-out-of-stock.php

--- public/email-notifications/mail-order-out-of-stock.php
+++ public/email-notifications/mail-order-out-of-stock.php
@@ -10,7 +10,7 @@ function mailOutOfStock($email,$orderNumber,$logo,$customerName,$orderDate,$orde
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'mangmacspizzahouse@gmail.com';
-    $mail->Password = 'ylzikpnelhxltves'; 
+    $mail->Password = ''; 
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
     $mail->setFrom('mangmacspizzahouse@gmail.com', "Mang Mac's Marinero");
