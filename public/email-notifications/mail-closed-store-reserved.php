03b5755332351180bf92c02aff95efc60b2a515d
Pam Patacsil <74691394+patacsilpam@users.noreply.github.com>
2022-12-17 15:51:43 +0800

    Update mail-closed-store-reserved.php

--- public/email-notifications/mail-closed-store-reserved.php
+++ public/email-notifications/mail-closed-store-reserved.php
@@ -11,7 +11,7 @@ function mailClosedStore($id,$email,$refNumber,$logo,$customerName,$guests,$crea
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'mangmacspizzahouse@gmail.com';
-    $mail->Password = 'ylzikpnelhxltves'; 
+    $mail->Password = ''; 
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
     $mail->setFrom('mangmacspizzahouse@gmail.com', "Mang Mac's Marinero");
